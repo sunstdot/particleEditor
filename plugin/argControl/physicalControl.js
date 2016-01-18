3b6fea01c95e304f24b46d35cbef4c2849f8c703
sunst <827489266@qq.com>
2016-01-18 19:02:01 +0800

    界面功能实现

---
+++ plugin/argControl/physicalControl.js
@@ -0,0 +1,11 @@
+/**
+ * Created by sunshitao on 2016/1/18.
+ */
+//import co from '../lib/index.js';
+
+function updateGravityInput(value){
+    document.getElementById("gravityValue").innerHTML = value;
+}
+function updateDynamicsInput(value){
+    document.getElementById("dynamicsValue").innerHTML = value;
+}
--- plugin/kinematics.js
+++ plugin/kinematics.js
@@ -1,9 +1,19 @@
+var myCanvas = document.getElementById("mainPainter");
+var ctx = myCanvas.getContext("2d");
+
+(function painterInit(){
+    ctx.fillStyle="rgba(0, 0, 0, 0.5)";
+    ctx.fillRect(0,0,myCanvas.width,myCanvas.height);
+}());
+
+
+
+
+
 var position = new vector2(10,200);
 var velocity = new vector2(50,-50);
 var acceleration = new vector2(0,10);
 var dt = 0.1;
-var myCanvas = document.getElementById("kinematics");
-var ctx = myCanvas.getContext("2d");
 function step(){
     position = position.add(velocity.multiply(dt));
     velocity = velocity.add(acceleration.multiply(dt));
@@ -23,5 +33,6 @@ var loop = function(){
         times--;
     },10)
 };
-loop();
+//loop();
+
 
