4dbad2bdca42b3eba5645d603b2be5091c0c537d
sunst <827489266@qq.com>
2016-05-05 10:47:00 +0800

    项目配置信息

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+var path = require('path');
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH,'app');
+var BUILD_PATH = path.resolve(ROOT_PATH,'build');
+var CSS_PATH = path.resolve(APP_PATH,'css');
+module.exports = {
+  entry:{
+    "app":APP_PATH+"/plugin/particleEditor"
+  },
+  output:{
+    path:BUILD_PATH,
+    publicPath:BUILD_PATH,
+    filename:'[name].js'
+  },
+  module:{
+    loaders:[
+      {
+        test:/\.jsx?$/,
+        loader:'babel',
+        include:[APP_PATH,path.resolve(ROOT_PATH, "node_modules/vue/src")],
+        query:{
+          presets:['es2015'],
+          compact:false
+        }
+      },
+      {
+        test:/\.css$/,
+        loader:'style!css',
+        include:APP_PATH
+      },
+      {
+        test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+        loader: 'url-loader?limit=50000&name=[path][name].[ext]'
+      },
+      {
+        test:require.resolve('jquery'),
+        loader:'expose?$!expose?jQuery'
+      }
+    ]
+  },
+  externals:{
+    '$':'jquery'
+  },
+  devServer:{
+    historyApiFallback:true,
+    hot:true,
+    inline:true,
+    progress:true
+  },
+  //devtool:'eval-source-map',
+  plugins:[
+  ],
+  resolve:{
+    root:ROOT_PATH,
+    alias: {
+      echarts$: "echarts/lib/echarts.js",
+      echarts: "echarts/lib",
+      zrender$: "zrender/lib/zrender.js",
+      zrender: "zrender/lib",
+      $:"jquery/jquery.js",
+      vue:"vue/src/index.js"
+    },
+    modulesDirectories:['node_modules']
+  }
+}
